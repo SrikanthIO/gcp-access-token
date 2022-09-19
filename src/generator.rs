827cc618ad53b1113c772e668afe4a49e0e40aaa
Srikanth Akkapu <srikanth.akkapu@cloudio.io>
2022-09-20 03:11:31 +0530

    #Added sub param in token request paylod

--- src/generator.rs
+++ src/generator.rs
@@ -1,60 +1,81 @@
-use reqwest::Client;
 use crate::token;
-use reqwest::{Response,Request};
-use urlencoding::encode as UrlEncodingEncode;
 use json::parse as JsonParse;
 use json::JsonValue;
+use reqwest::Client;
+use reqwest::{Request, Response};
+use urlencoding::encode as UrlEncodingEncode;
 
-pub async fn init(file_path:String,scope:String)->Result<JsonValue,&'static str>{
-
-    let jwt:String;
-    match token::init(file_path,scope).await{
-        Ok(v)=>{jwt = v;},
-        Err(_e)=>{return Err(_e);}
+pub async fn init(
+    sub: String,
+    file_path: String,
+    scope: String,
+) -> Result<JsonValue, &'static str> {
+    let jwt: String;
+    match token::init(sub, file_path, scope).await {
+        Ok(v) => {
+            jwt = v;
+        }
+        Err(_e) => {
+            return Err(_e);
+        }
     }
 
     // let form = Form::new()
     // .text("grant_type",UrlEncodingEncode("urn:ietf:params:oauth:grant-type:jwt-bearer"))
     // .text("assertion",jwt);
 
-    let body = format!("grant_type={}&assertion={}",
+    let body = format!(
+        "grant_type={}&assertion={}",
         UrlEncodingEncode("urn:ietf:params:oauth:grant-type:jwt-bearer"),
         jwt
     );
 
-    let request:Request;
+    let request: Request;
     match Client::new()
-    .post("https://oauth2.googleapis.com/token")
-    // .multipart(form)
-    .header("Content-Type","application/x-www-form-urlencoded")
-    .body(body)
-    .build(){
-        Ok(v)=>{request = v;},
-        Err(_)=>{return Err("failed-build_request");}
+        .post("https://oauth2.googleapis.com/token")
+        // .multipart(form)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .build()
+    {
+        Ok(v) => {
+            request = v;
+        }
+        Err(_) => {
+            return Err("failed-build_request");
+        }
     }
 
-    let response:Response;
-    match Client::new().execute(request).await{
-        Ok(v)=>{response = v;},
-        Err(_)=>{return Err("failed-send_request");}
+    let response: Response;
+    match Client::new().execute(request).await {
+        Ok(v) => {
+            response = v;
+        }
+        Err(_) => {
+            return Err("failed-send_request");
+        }
     }
 
-    let response_code:u16 = response.status().as_u16();
+    let response_code: u16 = response.status().as_u16();
     let response_string;
-    match response.text().await{
-        Ok(v)=>{response_string = v;},
-        Err(_)=>{return Err("failed-get-response-string");}
+    match response.text().await {
+        Ok(v) => {
+            response_string = v;
+        }
+        Err(_) => {
+            return Err("failed-get-response-string");
+        }
     }
 
     // println!("code : {}",response_code);
     // println!("body : {}",response_string);
 
-    if response_code != 200{
+    if response_code != 200 {
         return Err("request-failed");
     }
 
-    match JsonParse(&response_string){
-        Ok(token)=>{
+    match JsonParse(&response_string) {
+        Ok(token) => {
             return Ok(token);
             // if !token.has_key("access_token"){return Err("invalid-response_json");}
             // match token["access_token"].as_str(){
@@ -63,10 +84,9 @@ pub async fn init(file_path:String,scope:String)->Result<JsonValue,&'static str>
             //     },
             //     None=>{return Err("not_found-access_token-response_json");}
             // }
-        },
-        Err(_)=>{
+        }
+        Err(_) => {
             return Err("failed-parse_response_json");
         }
     }
-
 }
